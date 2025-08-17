1646b7e5225157b53f86cc3b53970ce87a6198f5
nkosana999 <pnnkosi17@gmail.com>
2025-08-16 19:17:32 -0700

    Updated plate.py with bug fix

--- plates.py
+++ plates.py
@@ -7,32 +7,31 @@ def main():
 
 
 def is_valid(s):
-    
-    if not (2 <= len(s) <= 6):
-        return False
 
-    if not (s[0].isalpha() and s[1].isalpha()):
-        return False
+    # Checking the first three mandatory requirements| 1 - length | 2 - first two characters are letters | 3 - All characters are letters or numbers.
+    if 2 <= len(s) <= 6 and s[:2].isalpha():
+        if s[:].isalnum():
 
-    num_list = []
-    for i in s:
-        if i.isdigit():
-            num_list.append(i)
-
-    if "0" in num_list:
-        if num_list[0] == "0":
-            return False
-    elif not (len(num_list) == 0):
-        if (s[-1].isalpha()):
-            return False
-        else:
+            # loop to check for numbers and ensuring there is no letter after a number.
+            num_list = []
+            for char in s[:]:
+                if char.isdigit():
+                    num_list.append(char)
+                    try:
+                        if s[s.index(char) + 1].isalpha():
+                            return False
+                    except IndexError:
+                        continue
+            if len(num_list) > 0:
+                if num_list[0] == "0":
+                    return False
             return True
 
-        
-    for i in s:
-        if not (i.isalpha()):
-            return False
         else:
-            return True
+            return False
+
+    else:
+        return False
+
 
 main()
